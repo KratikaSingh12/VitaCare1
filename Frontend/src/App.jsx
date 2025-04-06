3b5b8bbaaff1dba06ac5f9dbf15dceed1e9afb7c
Komalsingh7 <komalsingh742004@gmail.com>
2025-04-07 04:41:45 +0530

    Patient login and registration done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,12 +12,16 @@ import Contact from './pages/Contact';
 import Login from './pages/Login';
 import MyProfile from './pages/MyProfile';
 import MyAppointments from './pages/MyAppointments';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
   //console.log("apin");
   return (
     
     
     <div className="mx-4 sm:mx-[10%]">
+      <ToastContainer/>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
